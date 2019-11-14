a99ec91c898073121342311160af66a002b6b3ed
tplamondon <tplamondon@tutanota.de>
2019-11-13 16:22:58 -0800

    added basics of adding tiles

---
+++ Assets/Mouse_Click.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Mouse_Click : MonoBehaviour
+{
+
+    public Sprite[] list = new Sprite[21];
+    public int spriteToDo = (int)TileEnum.HOME;
+
+    int[,] tiles = new int[50, 30];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("Pressed primary button.");
+            if(Input.mousePosition.x > 200)
+            {
+                Debug.Log(Input.mousePosition.ToString());
+            }
+            
+
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Pressed secondary button.");
+        }
+            
+    }
+
+
+    public void changeSprite()
+    {
+        var drop = GameObject.Find("TileChoose").GetComponent<Dropdown>();
+        //Debug.Log(drop.value);
+        spriteToDo = drop.value;
+    }
+}
---
+++ Assets/TileEnum.cs
@@ -0,0 +1,26 @@
+﻿
+
+public enum TileEnum
+{
+    HOME,
+    STAR,
+    STAR2,
+    DRAW,
+    DRAW2,
+    BATTLE,
+    BATTLE2,
+    DROP,
+    DROP2,
+    WARP,
+    WARPMOVE,
+    WARPMOVE2,
+    MOVE,
+    MOVE2,
+    ICE,
+    HEAL,
+    HEAL2,
+    LEFT,
+    UP,
+    DOWN,
+    RIGHT
+}
