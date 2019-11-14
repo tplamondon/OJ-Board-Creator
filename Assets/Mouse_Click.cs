31abe9f4543e2e7e4e8ce1013792916855166739
tplamondon <tplamondon@tutanota.de>
2019-11-13 17:30:17 -0800

    now can work to scale

--- Assets/Mouse_Click.cs
+++ Assets/Mouse_Click.cs
@@ -7,9 +7,8 @@ using UnityEngine.UI;
 public class Mouse_Click : MonoBehaviour
 {
 
-    public Sprite[] list = new Sprite[21];
+    public Sprite[] spriteList = new Sprite[21];
     public int spriteToDo = (int)TileEnum.HOME;
-
 
     //36.? x len, 36 y len
     readonly int xMax = 47;
@@ -52,6 +51,8 @@ public class Mouse_Click : MonoBehaviour
                 //Debug.Log(Input.mousePosition.ToString());
             }
             int xPos = (int)Input.mousePosition.x;
+            int x = (int)Input.mousePosition.x;
+            int y = (int)Input.mousePosition.y;
             if (xPos < 200)
             {
                 return;
@@ -66,7 +67,29 @@ public class Mouse_Click : MonoBehaviour
                 return;
             }
             tiles[xPos, yPos] = spriteToDo;
+
+            if (objects[xPos, yPos] != null)
+            {
+                Destroy(objects[xPos, yPos]);
+            }
+
+            //36.? x len, 36 y len
             
+
+            //create the object
+            GameObject obj = new GameObject();
+            obj.AddComponent<SpriteRenderer>();
+            obj.GetComponent<SpriteRenderer>().sprite = spriteList[spriteToDo];
+
+            var rect = GUIRectWithObject(obj);
+            obj.GetComponent<Transform>().localScale = new Vector3(36/rect.width , 36/rect.height , 1);
+
+            Vector3 point = new Vector3();
+            point = Camera.main.ScreenToWorldPoint(new Vector3((xPos*36)+200, yPos*36, Camera.main.nearClipPlane));
+            Debug.Log(point.x+", "+point.y);
+            obj.transform.position = new Vector3(point.x, point.y, 0);
+            objects[xPos, yPos] = obj;
+
 
             
 
@@ -103,6 +126,37 @@ public class Mouse_Click : MonoBehaviour
             
     }
 
+    public static Rect GUIRectWithObject(GameObject go)
+    {
+        Vector3 cen = go.GetComponent<Renderer>().bounds.center;
+        Vector3 ext = go.GetComponent<Renderer>().bounds.extents;
+        Vector2[] extentPoints = new Vector2[8]
+         {
+               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z-ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z-ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z+ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z+ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z-ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z-ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z+ext.z)),
+               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z+ext.z))
+         };
+        Vector2 min = extentPoints[0];
+        Vector2 max = extentPoints[0];
+        foreach (Vector2 v in extentPoints)
+        {
+            min = Vector2.Min(min, v);
+            max = Vector2.Max(max, v);
+        }
+        return new Rect(min.x, min.y, max.x - min.x, max.y - min.y);
+    }
+
+    public static Vector2 WorldToGUIPoint(Vector3 world)
+    {
+        Vector2 screenPoint = Camera.main.WorldToScreenPoint(world);
+        screenPoint.y = (float)Screen.height - screenPoint.y;
+        return screenPoint;
+    }
 
     public void changeSprite()
     {
